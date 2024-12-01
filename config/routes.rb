f39fbaf738af4058a3cce4e3b8f70a0401cf7899
peyton owens <peyton21owens@gmail.com>
2024-12-01 18:11:27 -0500

    added show page for users, added controllers from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   resources :posts
-  devise_for :users
+
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  get "users/:id" => "users#show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "posts#index"
